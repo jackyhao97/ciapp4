a7648d118b2b544053f71f0953ebae8a151b15a9
Jacky <jackyhau97@gmail.com>
2021-08-16 10:29:09 +0700

    Tambah edit data.

--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -16,9 +16,9 @@
               <p class="card-text"><b>Penulis : </b> <?= $komik['penulis']; ?></p>
               <p class="card-text"><small class="text-muted"><b>Penerbit : </b> <?= $komik['penerbit']; ?></small></p>
 
-              <a href="" class="btn btn-warning">Edit</a>
+              <a href="<?= base_url('/komik/edit/'.$komik['slug']); ?>" class="btn btn-warning">Edit</a>
               
-              <form action="/komik/<?= $komik['id']; ?>" method="post" class="d-inline">
+              <form action="<?= base_url('/komik/'.$komik['id']); ?>" method="post" class="d-inline">
                 <?= csrf_field(); ?>
                 <input type="hidden" name="_method" value="DELETE">
                 <button type="submit" class="btn btn-danger" onclick="return confirm('apakah anda yakin?')">Delete</button>
